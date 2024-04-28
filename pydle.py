9d4864078e58fd9684b2112fd576451a9ab1006a
toboooo <60663629+toboooo@users.noreply.github.com>
2024-04-28 15:32:20 +0100

    Double letter colour fix?
    Hopefully, orange coloring when words have two or more of the same letter is now working correctly.

--- pydle.py
+++ pydle.py
@@ -10,6 +10,7 @@ from tkinter import *
 def input_callback(*args):
 	global attempt
 	global letters
+	global word_entry
 	# Check if number of allowed attempts has not been reached and the input is
 	# allowed.
 	guess = word_var.get().lower().strip(" ")
@@ -56,7 +57,6 @@ def input_callback(*args):
 
 	# Collect stats if correct.
 	if guess == answer_word:
-		print(answer_word)
 		stats["n_wins"] += 1
 		stats["attempt_distribution"][attempt-1] += 1
 		stats["score_sum"] += attempt
@@ -64,14 +64,17 @@ def input_callback(*args):
 		stats_file = open("stats_pydle.pkl", "wb")
 		pickle.dump(stats, stats_file)
 		stats_file.close()
-	elif attempt > 6:
-		print(answer_word)
+		word_entry.config(state="disabled")
+		root.unbind("<Return>")
+	elif attempt > 5:
+		print("Answer:", answer_word)
+		word_entry.config(state="disabled")
+		root.unbind("<Return>")
 
 	# Clear the text box after finished with word_var.
 	word_entry.delete(0, END)
 
-
-# Create the popup window for stats
+# Function to create the popup window for stats
 def popup_window():
 	stats_window = tkinter.Toplevel()
 	stats_text = "Number of plays: " + str(stats["n_plays"]) + "\n" + \
@@ -88,90 +91,148 @@ def popup_window():
 	for child in stats_window.winfo_children():
 		child.grid_configure(padx=5, pady=5)
 
-
-
-
-
-
-############################# STATS ##################################
-# Load in stats, create the stats dictionary if no file
-if os.path.exists("./stats_pydle.pkl"):
-	stats_file = open("stats_pydle.pkl", "rb")
-	stats = pickle.load(stats_file)
+# Function to reset the game after it is complete
+def reset_game():
 	stats["n_plays"] += 1
-	stats_file.close()
-else:
-	stats = {"n_plays": 1, "n_wins": 0, "score_sum": 0,
-			"attempt_distribution": [0,0,0,0,0,0]}
+	global root
+	global main_frame 
+	global input_frame
+	global words_frame
+	global word_var
+	global word_entry
+	global untried_letters
+	global word_attempts
+	global stats_button
+	global attempt
+	global answer_word
+	global letters
+	# Choose a new answer
+	answer_word = random.choice(answers)
+	# Reset number of attempts
+	attempt = 0
+	
+	# Create frame widget to hold GUI contents.
+	main_frame = ttk.Frame(root, padding="3 3 12 12")
+	main_frame.grid(column=0, row=0, sticky=(N,W,E,S))
+	root.columnconfigure(0, weight=1)
+	root.rowconfigure(0, weight=1)
 
-############################# WORDS ##################################
-# Get the word dictionaries.
-with open("wordle_dicts.csv") as file:
-	dicts = list(csv.reader(file))
-	file.close()
-answers, allowed_words = dicts[0], set(dicts[1])
-for answer in answers:
-	allowed_words.add(answer)
+	# Create a separate frame for the input box and letter tracking.
+	input_frame = ttk.Frame(main_frame)
+	input_frame.grid(column=0, row=1)
+	# Make the entry text box.
+	word_var = StringVar()
+	word_entry = ttk.Entry(input_frame, font=("Arial 30"), width=6, textvariable=word_var)
+	word_entry.grid(column=2, row=7)
 
-# Select a random word as the answer
-answer_word = random.choice(answers)
-#answer_word = "abide"
-#answer_word = "erase"
-#answer_word = "steal"
-#answer_word = "crepe"
-#print(answer_word)
+	# Make a widget for the list of letters.
+	letters = "Q W E R T Y U I O P\n  A S D F G H J K L\n    Z X C V B N M"
+	untried_letters = ttk.Label(input_frame, font=("Arial 22"), text=letters)
+	untried_letters.grid(column=2, row=8)
 
-# Initialise the number of attempts
-attempt = 0
+	# Create a new frame for the letters.
+	words_frame = ttk.Frame(main_frame)
+	words_frame.grid(column=0, row=0)
+	# Make the label objects that will contain the letters of the words.
+	# Five labels per word in a row, by six words
+	word_attempts = []
+	for i in range(6):
+		letter_attempts = []
+		for j in range(0, 5):
+			letter_attempt = ttk.Label(words_frame, font=("Arial 30"), text="")
+			letter_attempt.grid(column=j, row=i+1)
+			letter_attempts.append(letter_attempt)
+		word_attempts.append(letter_attempts)
 
-############################ CALLBACK ###################################
+	# Some formatting.
+	for child in main_frame.winfo_children():
+		child.grid_configure(padx=5, pady=5)
 
-############################# GUI ##################################
-# Make the game gui.
-root = tkinter.Tk()
-root.title("Wordle in Python")
+	# Reset button.
+	reset_button = ttk.Button(input_frame, text="Reset", command=reset_game)
+	reset_button.grid(column=2, row=9)
 
-# Create frame widget to hold GUI contents.
-main_frame = ttk.Frame(root, padding="3 3 12 12")
-main_frame.grid(column=0, row=0, sticky=(N,W,E,S))
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
+	# The button to show stats
+	stats_button = ttk.Button(input_frame, text="Stats", command=popup_window)
+	stats_button.grid(column=2, row=10)
 
-# Create a separate frame for the input box and letter tracking.
-input_frame = ttk.Frame(main_frame)
-input_frame.grid(column=0, row=1)
-# Make the entry text box.
-word_var = StringVar()
-word_entry = ttk.Entry(input_frame, font=("Arial 30"), width=6, textvariable=word_var)
-word_entry.grid(column=2, row=7)
-
-# Make a widget for the list of letters.
-letters = "Q W E R T Y U I O P\n  A S D F G H J K L\n    Z X C V B N M"
-untried_letters = ttk.Label(input_frame, font=("Arial 22"), text=letters)
-untried_letters.grid(column=2, row=8)
-
-# Create a new frame for the letters.
-words_frame = ttk.Frame(main_frame)
-words_frame.grid(column=0, row=0)
-# Make the label objects that will contain the letters of the words.
-# Five labels per word in a row, by six words
-word_attempts = []
-for i in range(6):
-	letter_attempts = []
-	for j in range(0, 5):
-		letter_attempt = ttk.Label(words_frame, font=("Arial 30"), text="")
-		letter_attempt.grid(column=j, row=i+1)
-		letter_attempts.append(letter_attempt)
-	word_attempts.append(letter_attempts)
-
-# Some formatting.
-for child in main_frame.winfo_children():
-	child.grid_configure(padx=5, pady=5)
+	# Rebind return key
+	root.bind("<Return>", input_callback)
 
 
-# The button to show stats
-stats_button = ttk.Button(input_frame, text="Stats", command=popup_window)
-stats_button.grid(column=2, row=9)
+if __name__ == "__main__":
+	# Load in stats, create the stats dictionary if no file
+	if os.path.exists("./stats_pydle.pkl"):
+		stats_file = open("stats_pydle.pkl", "rb")
+		stats = pickle.load(stats_file)
+		stats["n_plays"] += 1
+		stats_file.close()
+	else:
+		stats = {"n_plays": 1, "n_wins": 0, "score_sum": 0,
+				"attempt_distribution": [0,0,0,0,0,0]}
 
-root.bind("<Return>", input_callback)
-root.mainloop()
+	# Get the word dictionaries.
+	with open("wordle_dicts.csv") as file:
+		dicts = list(csv.reader(file))
+		file.close()
+	answers, allowed_words = dicts[0], set(dicts[1])
+	for answer in answers:
+		allowed_words.add(answer)
+
+	# Select a random word as the answer
+	answer_word = random.choice(answers)
+
+	# Initialise the number of attempts
+	attempt = 0
+
+	# Make the game gui.
+	root = tkinter.Tk()
+	root.title("Wordle in Python")
+
+	# Create frame widget to hold GUI contents.
+	main_frame = ttk.Frame(root, padding="3 3 12 12")
+	main_frame.grid(column=0, row=0, sticky=(N,W,E,S))
+	root.columnconfigure(0, weight=1)
+	root.rowconfigure(0, weight=1)
+
+	# Create a separate frame for the input box and letter tracking.
+	input_frame = ttk.Frame(main_frame)
+	input_frame.grid(column=0, row=1)
+	# Make the entry text box.
+	word_var = StringVar()
+	word_entry = ttk.Entry(input_frame, font=("Arial 30"), width=6, textvariable=word_var)
+	word_entry.grid(column=2, row=7)
+
+	# Make a widget for the list of letters.
+	letters = "Q W E R T Y U I O P\n  A S D F G H J K L\n    Z X C V B N M"
+	untried_letters = ttk.Label(input_frame, font=("Arial 22"), text=letters)
+	untried_letters.grid(column=2, row=8)
+
+	# Create a new frame for the letters.
+	words_frame = ttk.Frame(main_frame)
+	words_frame.grid(column=0, row=0)
+	# Make the label objects that will contain the letters of the words.
+	# Five labels per word in a row, by six words
+	word_attempts = []
+	for i in range(6):
+		letter_attempts = []
+		for j in range(0, 5):
+			letter_attempt = ttk.Label(words_frame, font=("Arial 30"), text="")
+			letter_attempt.grid(column=j, row=i+1)
+			letter_attempts.append(letter_attempt)
+		word_attempts.append(letter_attempts)
+
+	# Some formatting.
+	for child in main_frame.winfo_children():
+		child.grid_configure(padx=5, pady=5)
+
+	# Reset button.
+	reset_button = ttk.Button(input_frame, text="Reset", command=reset_game)
+	reset_button.grid(column=2, row=9)
+
+	# The button to show stats.
+	stats_button = ttk.Button(input_frame, text="Stats", command=popup_window)
+	stats_button.grid(column=2, row=10)
+
+	root.bind("<Return>", input_callback)
+	root.mainloop()
